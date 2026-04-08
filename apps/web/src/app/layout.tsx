c359441dff941ec008c376e06a551bf8e25e0563
Daniel Nwachukwu <dannyclassic56@gmail.com>
2026-04-08 12:45:37 +0100

    Update layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -11,6 +11,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'my-minipay-app',
   description: 'A new Celo blockchain project',
+  verification: {
+    other: {
+      "talentapp:project_verification":
+        "6a368db1fd3f1d17f2e13079b423c795680adad0a0ef39096784b7216db16a8d0ad9de7a42b0a0476ed43ca2a0a91d6cbc2503f5c9c3b4a7a661e8f0fb33931c",
+    },
+  },
 };
 
 export default function RootLayout({
